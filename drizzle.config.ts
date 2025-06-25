f009252c2ea19e77f07acab3d7b7e8b9187e386d
Roukaya <roukayachelly7@gmail.com>
2025-06-25 16:20:13 +0100

    refactor: update schema path in drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,8 @@
-import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
 export default {
   out: "./drizzle",
-  schema: "./src/db/schema.ts",
+  schema: "./db/schema.ts", 
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
